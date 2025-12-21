bfaf57712cb811a6153ffc7583ab631be8d01ae3
YeBeen <124907972+YeBeenChoi@users.noreply.github.com>
2025-12-21 14:02:21 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+// 11279
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); 
+  cout.tie(NULL);
+  
+  priority_queue<int> pq;
+
+  int n, x;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+    if (x > 0) {
+      pq.push(x);
+    } else if (x == 0) {
+      if (pq.empty()) {
+        cout << '0' << '\n';
+      } else {
+        cout << pq.top() << '\n';
+        pq.pop();
+      }
+    }
+  }
+}
